058bac623a9a2d242b9e09f964b9f7dc926a7a81
Nicholas Young <ynickd@gmail.com>
2017-01-23 18:44:59 -0800

    Added support for a config file

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ setup(
     version='0.1',
     description='For those that need fresh beegees.',
     url='https://github.com/BoltedLabs/beegees',
+    install_requires=[
+        'click'
+    ],
     scripts=['bins/beegees']
 )
